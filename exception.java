b2b44942e7cce4b3055c98170c809566f41f21ae
Subhani-syed <subhani6352@gmail.com>
2022-03-01 20:02:35 +0530

    Exception Handling

--- exception.java
+++ exception.java
@@ -4,7 +4,9 @@ class exception{
         Scanner sc= new Scanner(System.in);
         int res=0;
         try{
+            System.out.println("Enter a ");
             int a = Integer.parseInt(sc.nextLine());
+            System.out.println("Enter b ");
             int b = Integer.parseInt(sc.nextLine());
             res = a/b;
         }catch(ArithmeticException e){
