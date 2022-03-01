9756cec1ca39f68e9ddda369eb3556227f0b3bba
Subhani-syed <subhani6352@gmail.com>
2022-03-01 20:23:27 +0530

    Basic Operations on ArrayList

--- arraylist.java
+++ arraylist.java
@@ -1,15 +1,18 @@
 import java.util.*;
 class arraylist{
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
         ArrayList<Integer> arr;
         arr = new ArrayList<>();
+        System.out.println("Enter the elements to be added into arraylist :");
         for(int i=0;i<5;i++){
-            arr.add(1);
+            arr.add(sc.nextInt());
         }
+        System.out.println("The Elements are: ");
         for(int i=0;i<5;i++){
-            // arr.add(1);
             System.out.println(arr.get(i));
         }
+        System.out.print("The Size of list is : ");
         System.out.println(arr.size());
     }
 }
