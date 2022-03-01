cb5336987f13c58e40ca87e38f3700b8063097c9
Subhani-syed <subhani6352@gmail.com>
2022-03-01 20:24:45 +0530

    Basic Operations on ArrayList

--- arraylist.java
+++ arraylist.java
@@ -4,15 +4,27 @@ class arraylist{
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> arr;
         arr = new ArrayList<>();
-        System.out.println("Enter the elements to be added into arraylist :");
-        for(int i=0;i<5;i++){
-            arr.add(sc.nextInt());
+        try{
+            System.out.println("Enter the elements to be added into arraylist :");
+            for(int i=0;i<5;i++){
+                arr.add(sc.nextInt());
+            }
+            System.out.println("The Elements are: ");
+            for(int i=0;i<arr.size();i++){
+                System.out.println(arr.get(i));
+            }
+            System.out.print("The Size of list is : ");
+            System.out.println(arr.size());
+            System.out.println("Removing the element at index 1");
+            arr.remove(1);
+            System.out.println("The Elements are: ");
+            for(int i=0;i<arr.size();i++){
+                System.out.println(arr.get(i));
+            }
+            System.out.print("The Size of list is : ");
+            System.out.println(arr.size());
+        }catch(Exception e){
+            System.out.println(e);
         }
-        System.out.println("The Elements are: ");
-        for(int i=0;i<5;i++){
-            System.out.println(arr.get(i));
-        }
-        System.out.print("The Size of list is : ");
-        System.out.println(arr.size());
     }
 }
